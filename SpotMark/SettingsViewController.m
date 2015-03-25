3781c0317aad354ff06731a6f3a813c918a838cd
rafaelabertolini <bertolinifr@gmail.com>
2015-03-25 14:07:17 -0300

    Novas classes e novas view controllers

---
+++ SpotMark/SettingsViewController.m
@@ -0,0 +1,39 @@
+//
+//  SettingsViewController.m
+//  SpotMark
+//
+//  Created by Rafaela dos Santos Bertolini on 25/03/15.
+//  Copyright (c) 2015 Lucas Fraga Schuler. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName: [UIColor colorWithRed:1 green:0.97 blue:0.84 alpha:0.70]};
+    self.title = @"Settings";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
